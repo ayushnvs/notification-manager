87c758a857b8d7d714bb86b7a135ce253d0f9962
ayushnvs <ayush.mishra.ald@gmail.com>
2024-12-05 22:24:54 +0530

    Request Notification Listener Permission

--- NotificationManager/Platforms/Android/MainActivity.cs
+++ NotificationManager/Platforms/Android/MainActivity.cs
@@ -1,7 +1,10 @@
 ﻿using Android.App;
+using Android.Content;
 using Android.Content.PM;
 using Android.OS;
 using Android.Provider;
+using Android.Text;
+using Android.Widget;
 
 namespace NotificationManager;
 
@@ -17,6 +20,18 @@ public class MainActivity : MauiAppCompatActivity
 
     private void RequestNotificationListenerPermission()
     {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.SetTitle("Permission Required");
+        builder.SetMessage("Please enable notification listener access for this app.");
+        builder.SetPositiveButton("Go to Settings", (sender, args) =>
+        {
+            Intent intent = new Intent(Settings.ActionNotificationListenerSettings);
+            StartActivity(intent);
+        });
+        builder.SetNegativeButton("Cancel", (sender, args) =>
+        {
+            Toast.MakeText(this, "Permission denied. The app will not function properly.", ToastLength.Short)?.Show();
+        });
 
     }
 
@@ -24,6 +39,15 @@ public class MainActivity : MauiAppCompatActivity
     {
         string? packageName = PackageName;
         string? flat = Settings.Secure.GetString(ContentResolver, "enabled_notification_listeners");
+
+        if (string.IsNullOrEmpty(flat)) return false;
+
+        string[] names = flat.Split(':');
+        foreach (string name in names) 
+        {
+            ComponentName? componentName = ComponentName.UnflattenFromString(name);
+            if (componentName != null && TextUtils.Equals(packageName, componentName.PackageName)) return true;
+        }
 
         return false;
     }

36351051255c45ff0a3c6cf962f3baaddcd33cec
ayushnvs <ayush.mishra.ald@gmail.com>
2024-12-07 12:34:17 +0530

    Update project to .net 8

---
+++ NotificationManager/Platforms/Android/Services/NotificationReceiverService.cs
@@ -0,0 +1,21 @@
+﻿using Android.App;
+using Android.Content;
+
+namespace NotificationManager.Platforms.Android.Services;
+
+[BroadcastReceiver(Enabled = true, Permission = "android.Manifest.Permission.RECEIVE_BOOT_COMPLETED")]
+[IntentFilter(["intentNotificationRecieved"])]
+public class NotificationReceiverService : BroadcastReceiver
+{
+    public override void OnReceive(Context? context, Intent? intent)
+    {
+        if (intent == null) return;
+
+        string? title = intent.GetStringExtra("title");
+        string? text = intent.GetStringExtra("text");
+        long? timestamp = intent.GetLongExtra("timestamp", 0);
+        string? appName = intent.GetStringExtra("appName");
+
+
+    }
+}

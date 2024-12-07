a076e5f9cf46b4bdb0d2972402a84bbe0d7419b1
ayushnvs <ayush.mishra.ald@gmail.com>
2024-12-07 22:36:42 +0530

    Update Notification Repository

--- NotificationManager/Repository/Interfaces/INotificationRepository.cs
+++ NotificationManager/Repository/Interfaces/INotificationRepository.cs
@@ -4,8 +4,9 @@ namespace NotificationManager.Repository.Interfaces;
 
 public interface INotificationRepository
 {
-    Task<List<NotificationDBO>> GetNotificationsAsync();
+    Task<List<NotificationDBO>> GetNotificationsAsync(string? appName);
     Task<NotificationDBO> GetNotificationAsync(Guid id);
     Task<int> SaveNotificationAsync(NotificationDBO item);
     Task<int> DeleteNotificationAsync(Guid id);
+    Task<List<string?>> GetUniqueAppNamesAsync();
 }
--- NotificationManager/Repository/NotificationRepository.cs
+++ NotificationManager/Repository/NotificationRepository.cs
@@ -1,4 +1,5 @@
-﻿using NotificationManager.Database;
+﻿using SQLite;
+using NotificationManager.Database;
 using NotificationManager.Entities.Models;
 using NotificationManager.Repository.Interfaces;
 
@@ -13,10 +14,15 @@ public class NotificationRepository : INotificationRepository
         _databaseContext = databaseContext;
     }
 
-    public async Task<List<NotificationDBO>> GetNotificationsAsync()
+    public async Task<List<NotificationDBO>> GetNotificationsAsync(string? appName)
     {
         await _databaseContext.InitializeDatabase();
-        return await _databaseContext.Database.Table<NotificationDBO>().ToListAsync();
+        AsyncTableQuery<NotificationDBO> query = _databaseContext.Database.Table<NotificationDBO>();
+        if (appName != null)
+        {
+            query = query.Where(notif => notif.NotificationApp == appName);
+        }
+        return await query.ToListAsync();
     }
 
     public async Task<NotificationDBO> GetNotificationAsync(Guid id)
@@ -46,5 +52,15 @@ public class NotificationRepository : INotificationRepository
         await _databaseContext.InitializeDatabase();
         NotificationDBO item = await GetNotificationAsync(id);
         return await _databaseContext.Database.DeleteAsync(item);
+    }
+
+    public async Task<List<string?>> GetUniqueAppNamesAsync()
+    {
+        await _databaseContext.InitializeDatabase();
+
+        List<NotificationDBO> appList = await _databaseContext.Database
+            .Table<NotificationDBO>().ToListAsync();
+
+        return appList.Select(app => app.NotificationApp).Distinct().ToList();
     }
 }

b4af156c6a46d48650c75d4ac606704e42b447ed
ayushnvs <ayush.mishra.ald@gmail.com>
2025-01-27 10:50:39 +0530

    Updates

---
+++ NotificationManager.Hybrid/CSHelper/Helper.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace NotificationManager.Hybrid.CSHelper;
+
+public class Helper
+{
+    public delegate void WithinFunc();
+    public delegate void AfterFunc();
+    public static void TimeFunction(WithinFunc within, AfterFunc after, int ms)
+    {
+        Task task = Task.Run(() => within());
+        if (task.Wait(TimeSpan.FromMilliseconds(ms)))
+        {
+            after();
+        }
+    }
+}
---
+++ NotificationManager.Hybrid/wwwroot/js/app.js
@@ -0,0 +1,6 @@
+﻿function toggleBackgroundColor(cssClass, clientX, clientY) {
+    var element = document.elementFromPoint(clientX, clientY);
+    if (element) {
+        element.classList.toggle(cssClass);
+    }
+}
